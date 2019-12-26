bdc3dcf9db0c9d51f6e2fe1b41c932af1764aa8b
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-12-26 22:00:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,3 +20,10 @@ song = Song.new_by_name("The Middle")
 song.name
 
 song = Song.create_by_name("The Middle")
+
+Song.find_by_name("The Middle")
+
+song_1 = Song.find_or_create_by_name("Blank Space")
+song_2 = Song.find_or_create_by_name("Blank Space")
+
+song_1 == song_2
